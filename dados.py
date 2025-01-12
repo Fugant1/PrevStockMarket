7e2a5d70f7cdb318e4bbb56ad1155163d2829587
Ivan Salina De Nicolai <101476624+IkkusHiro@users.noreply.github.com>
2025-01-12 10:33:02 -0300

    Update dados.py
    Added a function where you can enter the Ticker name to search it on Yahoo Finance and then create create a chart based on the providade data.

--- dados.py
+++ dados.py
@@ -1,17 +1,41 @@
-import yfinance as yf
+import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+import yfinance as yf
+import warnings
 
-tikcer = 'AAPL'
+def plot_monthly_mean_closing_prices(ticker_symbol):
 
-df = yf.download(tikcer, start='2020-01-01', end='2020-03-03')
+    data = yf.download(ticker_symbol, start='2020-01-01', end='2025-01-01')
 
-# plt.figure(figsize=(10, 6))
-# plt.scatter(df['Date'], df['Close'], label='Dados reais', alpha=0.5)
-# plt.xlabel('Dias desde o início')
-# plt.ylabel('Preço Ajustado')
-# plt.title('Regressão Linear do Preço das Ações da Apple')
-# plt.legend()
-# plt.show()
+    if data.empty: 
+        print(f"No data found for ticker symbol: {ticker_symbol}") 
+        return False
+    
+    # Divisão por mes, calculo de media da coluna 'Close'
+    data['Year'] = data.index.year
+    data['Month'] = data.index.month
+    data['YearMonth'] = data['Year'].astype(str) + '-' + data['Month'].astype(str).str.zfill(2)
+    monthly_mean = data.groupby('YearMonth')['Close'].mean()
 
-print (df[0])
+    # Grafico com a media do 'Close'
+    plt.figure(figsize=(10, 6))
+    plt.plot(monthly_mean.index, monthly_mean.values, marker='o', linestyle='-')
+    plt.title('Monthly Mean Closing Prices')
+    plt.xlabel('Month')
+    plt.ylabel('Mean Closing Price')
+    plt.xticks(rotation=90)
+    plt.grid(True)
+    plt.show()
+    return True
+
+if __name__ == '__main__':
+    while True:
+        ticker_symbol = input("Escreva o Ticker da ação: ")
+        success = plot_monthly_mean_closing_prices(ticker_symbol)
+        if not success:
+            choice = input("Nada foi encontrado, gostaria de tentar novamente? (y/n): ")
+            if choice.lower() != 'y':
+                break
+        else:
+            break
